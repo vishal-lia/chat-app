4a00906570e0e3051efb23d43f0234510540d8ed
vishal-lia <vishallia77@gmail.com>
2018-07-25 22:01:03 +0530

    Add styles

--- public/js/script.js
+++ public/js/script.js
@@ -30,26 +30,32 @@ socket.on('disconnect', () => {
 
 $('#message-form').on('submit', e => {
     e.preventDefault();
+    let messageTextbox = $('[name=message]');
 
     socket.emit('createMessage', {
         'from': 'User',
-        'text': $('[name=message]').val()
+        'text': messageTextbox.val()
     }, () => {
-
+        messageTextbox.val('');
     });
 });
 
-$('#send-location').on('click', () => {
+let locationButton = $('#send-location');
+locationButton.on('click', () => {
     if(!navigator.geolocation) {
         return alert('Geolocation not supported by you browser');
     }
+
+    locationButton.attr('disabled', 'disabled').text('Sending...');
 
     navigator.geolocation.getCurrentPosition(position => {
         socket.emit('createLocationMessage', {
             'latitude': position.coords.latitude,
             'longitude': position.coords.longitude
         });
+        locationButton.removeAttr('disabled').text('Send Location');        
     }, () => {
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location');
     });
 });
--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection', socket => {
     socket.on('createMessage', (message, callback) => {
         console.log('Create message:', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from server..');
+        callback();
     });
 
     socket.on('createLocationMessage', message => {
